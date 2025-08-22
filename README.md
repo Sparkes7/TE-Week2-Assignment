7b2b8c3e9062cf65601d834a14677033aae21bc9
Sparkes7 <willsparkes93@gmail.com>
2025-08-22 23:06:18 +0100

    JS feature: implemented full screen toggle to the gallery. Refactored repeat code into a single function "updateImage()" HTML/CSS: implemented basic styles and framework for the project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Reflection
+
+## Implemented
+
+### TO DO:
+
+🎯 Implement responsive design methods to ensure the website works well on both small mobile screens and larger desktop screens (e.g., above 800px).
+
+🎯 Implement at least one meaningful media query so that there is a noticeable change between mobile and desktop view (an example is to change where the ‘thumbnail bar’ is positioned).
+
+🎯 Ensure all images have appropriate alt text attributes for accessibility.
+
+🎯 Correctly use event handlers to switch images based on user interactions.
--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-console.log("hello world");
-
 const images = [
   {
     title: "A city at night",
     filePath: "./images/city-7358078_1920.jpg",
-    altText: "",
+    altText:
+      "A bustling Korean highrise cityscape at night lit by the cities light",
   },
   {
     title: "Buildings at night",
     filePath: "./images/buildings-1839647_1920.jpg",
-    altText: "",
+    altText: "Buildings lit at night",
+  },
+  {
+    title: "Industrial Snow",
+    filePath: "./images/lukas-lehotsky-ZEifAiol6Gk-unsplash.jpg",
+    altText: "A photo by Lukas Lehotsky",
   },
 ];
+
+for (i = 0; i < images.length; i++) {
+  images[i].index = i;
+}
+console.log(images);
 
 let imageToDisplay = 0;
 //const featuredImage = document.getElementById("featured-image");
@@ -41,8 +50,10 @@ const featuredInfo = document.createElement("div");
 featuredInfo.classList.add("featured-info");
 const featuredInfoTitle = document.createElement("strong");
 featuredInfoTitle.classList.add("image-title");
+featuredInfoTitle.textContent = images[imageToDisplay].title;
 const featuredInfoText = document.createElement("p");
 featuredInfoText.classList.add("image-text");
+featuredInfoText.textContent = images[imageToDisplay].altText;
 featuredInfo.appendChild(featuredInfoTitle);
 featuredInfo.appendChild(featuredInfoText);
 
@@ -65,31 +76,50 @@ featureContainer.appendChild(prevImageButton);
 featureContainer.appendChild(nextImageButton);
 featureContainer.appendChild(closeImageButton);
 
-//featuredImage.setAttribute("src", images[imageToDisplay].filePath);
+// initialise thumbnails
+for (i = 0; i < images.length; i++) {
+  const thumbnailContainer = document.getElementById("thumbnail-container");
+  const thumbnailDiv = document.createElement("div");
+  thumbnailDiv.classList.add("thumbnail");
+  const image = document.createElement("img");
+  image.classList.add("thumbnail-image");
+  image.setAttribute("data", i);
+  image.setAttribute("src", images[i].filePath);
+  image.addEventListener("click", function () {
+    imageToDisplay = image.getAttribute("data");
+    toggleImage();
+    console.log(imageToDisplay);
+  });
+
+  thumbnailDiv.appendChild(image);
+  thumbnailContainer.appendChild(thumbnailDiv);
+}
 
 // Buttons Functionality
 
 function nextImage() {
   if (imageToDisplay < images.length - 1) {
     imageToDisplay++;
-    featuredImage.setAttribute("src", images[imageToDisplay].filePath);
+    updateImage();
   } else if (imageToDisplay == images.length - 1) {
     imageToDisplay = 0;
-    featuredImage.setAttribute("src", images[imageToDisplay].filePath);
+    updateImage();
   }
 }
 
 function prevImage() {
   if (imageToDisplay > 0) {
     imageToDisplay--;
-    featuredImage.setAttribute("src", images[imageToDisplay].filePath);
+    updateImage();
   } else if (imageToDisplay === 0) {
     imageToDisplay = images.length - 1;
-    featuredImage.setAttribute("src", images[imageToDisplay].filePath);
+    updateImage();
   }
 }
 
 function toggleImage() {
+  updateImage();
+
   const featureSection = document.getElementById("feature");
 
   if (featureSection.classList.contains("hidden")) {
@@ -99,9 +129,11 @@ function toggleImage() {
   }
 }
 
-// const prevImageButton = document.getElementById("prev-image");
-// const nextImageButton = document.getElementById("next-image");
-// const closeImageButton = document.getElementById("close-image");
+function updateImage() {
+  featuredImage.setAttribute("src", images[imageToDisplay].filePath);
+  featuredInfoTitle.textContent = images[imageToDisplay].title;
+  featuredInfoText.textContent = images[imageToDisplay].altText;
+}
 
 nextImageButton.addEventListener("click", nextImage);
 prevImageButton.addEventListener("click", prevImage);
@@ -114,16 +146,5 @@ function handleKeyPress(key) {
     nextImage();
   } else if (key.key === "ArrowLeft") {
     prevImage();
-  } else if (key.key === "Escape") {
-    toggleImage();
   }
 }
-
-//
-// Might need later:
-
-// loop through images and give them an index key value pair.
-// for (i = 0; i < images.length; i++) {
-//   images[i].index = i;
-// }
-// console.log(images);
